63b9bdfb939fda55241f29877a370a901b67c80a
smartquailstage <smartquail.stage@gmail.com>
2025-02-17 18:25:29 -0500

    Update start.sh

--- qndpostfix/postfix/start.sh
+++ qndpostfix/postfix/start.sh
@@ -1,60 +1,18 @@
 #!/bin/bash
 
-# Configuración de las credenciales de PostgreSQL
-export PGPASSWORD="smartquaildev1719pass"
-export PGUSER="sqadmindb"
-export POSTFIX_POSTGRES_DB="POSFIXDB"
-export POSTFIX_POSTGRES_USER="sqadmindb"
-export POSTFIX_POSTGRES_HOST="smartquaildb"
+
 
 function log {
   echo "$(date) $ME - $@"
 }
 
-function serviceConf {
-  if [[ ! $HOSTNAME =~ \. ]]; then
-    HOSTNAME="$HOSTNAME.$DOMAIN"
-  fi
-
-  for VARIABLE in $(env | cut -f1 -d=); do
-    VAR=${VARIABLE//:/_}
-    VALUE=${!VAR}
-    sed -i "s|{{ $VAR }}|$VALUE|g" /etc/postfix/*.cf
-  done
-
-  if [ -f /overrides/postfix.cf ]; then
-    while read -r line; do
-      [[ -n "$line" && "$line" != [[:blank:]#]* ]] && postconf -e "$line"
-    done < /overrides/postfix.cf
-    echo "Loaded '/overrides/postfix.cf'"
-  else
-    echo "No extra postfix settings loaded because optional '/overrides/postfix.cf' not provided."
-  fi
-
-  if ls -A /overrides/*.map 1> /dev/null 2>&1; then
-    cp /overrides/*.map /etc/postfix/
-    postmap /etc/postfix/*.map
-    rm /etc/postfix/*.map
-    chown root:root /etc/postfix/*.db
-    chmod 0600 /etc/postfix/*.db
-    echo "Loaded 'map files'"
-  else
-    echo "No extra map files loaded because optional '/overrides/*.map' not provided."
-  fi
-}
-
 
 
 function serviceStart {
-  serviceConf
   log "[ Iniciando Postfix... ]"
   /usr/sbin/postfix start-fg
 }
 
-export DOMAIN=${DOMAIN:-"juansilvaphoto.com"}
-export HOSTNAME=${HOSTNAME:-"mailpost.juansilvaphoto.com"}
-export MESSAGE_SIZE_LIMIT=${MESSAGE_SIZE_LIMIT:-"50000000"}
-export RELAYNETS=${RELAYNETS:-""}
-export RELAYHOST=${RELAYHOST:-""}
+
 
 serviceStart &>> /proc/1/fd/1
