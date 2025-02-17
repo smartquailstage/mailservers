22b7dd15946be1778da76a11a1c32637d289e25f
smartquailstage <smartquail.stage@gmail.com>
2025-02-16 21:16:26 -0500

    Update start2.sh

--- qndpostfix/postfix/start2.sh
+++ qndpostfix/postfix/start2.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
 
+# Start OpenDKIM
+/usr/sbin/opendkim -x /etc/opendkim/opendkim.conf &
 
-
-function log {
-  echo "$(date) $ME - $@"
-}
-
-function setPermissions {
-  # Ensure directories and files have correct permissions
-  log "Setting permissions for Postfix directories and files..."
-
-  # Set ownership and permissions for Postfix directories
-  chown -R postfix:postfix /var/spool/postfix
-  chmod 750 /var/spool/postfix/private
-  chmod 750 /var/spool/postfix
-
-  # Set ownership and permissions for Postfix configuration files
-  chown -R root:root /etc/postfix
-  chmod 640 /etc/postfix/*.cf
-
-  # Set ownership and permissions for mail directories
-  chown -R postfix:postfix /var/mail/
-  chmod 700 /var/mail/
-
-}
-
-function serviceStart {
-
-  setPermissions
-  log "[ Iniciando Postfix... ]"
-  /usr/sbin/postfix start-fg
-}
-
-
-serviceStart &>> /proc/1/fd/1
+# Start Postfix
+/usr/sbin/postfix start-fg
