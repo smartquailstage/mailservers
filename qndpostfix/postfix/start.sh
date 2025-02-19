166715591a1d9694a5522e9fb49811c23aa29ac9
smartquailstage <smartquail.stage@gmail.com>
2025-02-19 16:35:20 -0500

    sss

--- qndpostfix/postfix/start.sh
+++ qndpostfix/postfix/start.sh
@@ -4,26 +4,9 @@ function log {
   echo "$(date) $ME - $@"
 }
 
-function setPermissions {
-  # Ensure directories and files have correct permissions
-  log "Setting permissions for Postfix directories and files..."
 
-  # Set ownership and permissions for Postfix directories
-  chown -R postfix:postfix /var/spool/postfix
-  chmod 750 /var/spool/postfix/private
-  chmod 750 /var/spool/postfix
-
-  # Set ownership and permissions for Postfix configuration files
-  chown -R root:root /etc/postfix
-  chmod 640 /etc/postfix/*.cf
-
-  # Set ownership and permissions for mail directories
-  chown -R postfix:postfix /var/mail/
-  chmod 700 /var/mail/
-}
 
 function serviceStart {
-  setPermissions
   log "[ Iniciando Postfix... ]"
   # Iniciar Postfix en segundo plano
   /usr/sbin/postfix start-fg &
